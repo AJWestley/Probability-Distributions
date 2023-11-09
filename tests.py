138a4667524d8c15b196119727c2d7020d051ab3
AJ Westley <alexanderjwestley@gmail.com>
2023-11-09 20:20:10 +0200

    Test for gaussian_dist

--- tests.py
+++ tests.py
@@ -15,6 +15,14 @@ class TestGPyT(unittest.TestCase):
             self.assertLessEqual(val, dist_max, "uniform_dist: generated value below the minimum bounds.")
             self.assertGreaterEqual(val, dist_min, "uniform_dist: generated value above the maximum bounds.")
         self.assertAlmostEqual(np.mean(a), (dist_max + dist_min) / 2, 1, "uniform_dist: generated distribution did not have the expected mean.")
+    
+    def test_gaussian_dist(self):
+        dist_length = 100000
+        dist_mu = 0
+        dist_sig = 5
+        a = distributions.gaussian_dist(dist_length, dist_mu, dist_sig)
+        self.assertAlmostEqual(np.mean(a), dist_mu, 1, "gaussian_dist: generated distribution did not have the expected mean.")
+        self.assertAlmostEqual(np.std(a), dist_mu, 1, "gaussian_dist: generated distribution did not have the expected standard deviation.")
 
 
 if __name__ == '__main__':
